33962f29db05b470725c2b8e4238fe517c0d4151
Sarah Severinghaus <sseveringhaus@reed.edu>
2025-12-02 13:52:33 -0800

    added comments

--- main/main.ino
+++ main/main.ino
@@ -1,27 +1,27 @@
 #include <Servo.h>
 #include "Adafruit_VL53L1X.h"
 
-// tof 
+// tof pins
 #define IRQ_PIN 8
 #define XSHUT_PIN 7
 
 Adafruit_VL53L1X vl53 = Adafruit_VL53L1X(XSHUT_PIN, IRQ_PIN);
 
-// stepper
+// stepper pins
 #define DIR_PIN 12
 #define PUL_PIN 13
 
 // servo
-
 Servo myservo;
 
 bool running = true;
 
-float de2 = 1000;
+float de2 = 1000; // step delays
 float de1 = 250;
 
-int n = 1;
+int n = 1; // n iterations of stepper drive signal
 
+// target distance and corresponding number of steps to take
 int dists[] = {760,770,780,790,800,810,820,830,840,850,860,870,880,890,900,910,920,930,940,950,960,970,980,990,1000,1010,1020,1030};
 int steps[] = {4,  5,  6,  7,  8,  12,  18,  20,  25,  25,  25,  25,  26,  29,  28,  28,  20,  20,  22,  25,  25,  25,  30,  30,  30,  40,  40,  40};
 
@@ -45,20 +45,20 @@ void setup() {
         }
 
         int dist = round10(distance);
-        int l = sizeof(dists<distance) / sizeof(dists[0]);
+        int l = sizeof(dists<distance) / sizeof(dists[0]); // match new measurement to corresponding step count in steps[]
         int step_count = steps[l];
 
-        for (int i = 0; i <= step_count; i++) {
+        for (int i = 0; i <= step_count; i++) { // make step_count number of steps
           delay(5+i);
           step();
           delay(40);
         }
 
-        delay(5000);
+        delay(5000); // delay, should make this a function of step_count to reduce unnecessary delay for small angle adjustments
         myservo.attach(4);
-        trigger();
+        trigger(); // trigger w/ servo
         delay(250);
-        untrigger();
+        untrigger(); // untrigger w/ servo
       }
    }
 }
@@ -106,7 +106,7 @@ void step() {
   }
 }
 
-int round10(int num){
+int round10(int num){ // round to the nearest power of 10
   int len = log10(num);
   float div = pow(10, len);
   int rounded = ceil(num / div) * div;

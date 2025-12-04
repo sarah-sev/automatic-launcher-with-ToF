a2d130c4a876b79d4b2b48a077e90255086e72f4
Sarah Severinghaus <sseveringhaus@reed.edu>
2025-12-03 17:10:37 -0800

    Update main.ino

--- main/main.ino
+++ main/main.ino
@@ -16,14 +16,20 @@ Servo myservo;
 
 bool running = true;
 
-float de2 = 1000; // step delays
-float de1 = 250;
+float de2 = 10; // step delays
+float de1 = 5;
 
-int n = 1; // n iterations of stepper drive signal
+int n = 3; // n iterations of stepper drive signal
 
 // target distance and corresponding number of steps to take
-int dists[] = {150,160,170,180,190,200,260,270,280,290,300,310,320,330,340,350,360,370,380,390, 400, 410, 420, 430, 440,450,460,470,480,490,500,510,520,530,540,550,560,570,580,600,610,620,630,640,650,660,670,680,690,700,710,720,730,740,750,760,770,780,790,800,810,820,830,840,850,860,870,880};
-int steps[] = {0, 0, 0, 0, 0, 0, 3, 3, 3, 3, 6, 6, 6, 6, 6, 6, 6, 6, 6,  9,  9,  9,  9,  9,  9, 12, 12, 12, 12, 12, 15, 15, 15, 15, 15, 18, 18, 18, 18, 18, 18, 21, 21, 21, 21, 21, 21, 24, 24, 24, 24,24, 24, 27, 27, 27, 27, 27, 30, 30, 30, 30, 30, 30};
+int dists[] = {150,160,170,180,190,200,260,270,280,290,300,310,320,330,340,350,360,370,380,390, 400, 
+              410, 420, 430, 440,450,460,470,480,490,500,510,520,530,540,550,560,570,580,600,610,620,
+              630,640,650,660,670,680,690,700,710,720,730,740,750,760,770,780,790,800,810,820,830,840,
+              850,860,870,880};
+int steps[] = { 0,  0,  0,  0,  1,  2,  2,  3,  3,  3,  5,  5,  5,  6,  6,  6,  6,
+        6,  6,  8,  8,  9,  9,  9,  10, 10, 10, 10, 11, 11, 12, 12, 13, 14,
+       15, 16, 16, 17, 18, 20, 22, 22, 22, 23, 23, 24, 25, 26, 26, 26, 28,
+       28, 28, 29, 29, 30, 32, 32, 35, 35, 36, 38, 39, 40};
 
 void setup() {
   
@@ -37,13 +43,12 @@ void setup() {
    if(running) {
       if (vl53.dataReady()) {
         int16_t distance1 = vl53.distance();
-        Serial.println(distance1);
+
         delay(50);
         int16_t distance2 = vl53.distance();
-        Serial.println(distance2);
+
         delay(50);
         int16_t distance3 = vl53.distance();
-        Serial.println(distance3);
 
         int16_t distance_arr[] ={distance1,distance2,distance3};
         
@@ -58,20 +63,36 @@ void setup() {
         }
 
         int dist = round10(distance);
-        int l = sizeof(dists<distance) / sizeof(dists[0]); // match new measurement to corresponding step count in steps[]
-        int step_count = steps[l];
 
-        for (int i = 0; i <= step_count; i++) { // make step_count number of steps
-          delay(5+i);
+        int len = sizeof(dists) / sizeof(dists[1]);
+        delay(100);
+
+        delay(100);
+        int i=0;
+        while (dists[i] <= dist){i++;
+        }
+// match new measurement to corresponding step count in steps[]    
+        int step_count = steps[i];
+        Serial.println(step_count);   
+
+
+        for (int i = 0; i <= step_count; i++) { 
+          delay(10+i);
           step();
-          delay(40);
+          delay(3);
         }
 
-        delay(5000); // delay, should make this a function of step_count to reduce unnecessary delay for small angle adjustments
+        delay(2000+step_count*10); // delay, should make this a function of step_count to reduce unnecessary delay for small angle adjustments
         myservo.attach(4);
         trigger(); // trigger w/ servo
         delay(250);
         untrigger(); // untrigger w/ servo
+        delay(450);
+        for (int i = 0; i <= step_count+15; i++) { 
+          delay(10+i);
+          unstep();
+          delay(3);
+        }
       }
    }
 }
@@ -97,10 +118,9 @@ void tof_init() {
 }
 
 void unstep() {
-  digitalWrite(DIR_PIN,LOW); 
-  delayMicroseconds(5);
-
   for (int i = 0; i <= n; i++) {
+    digitalWrite(DIR_PIN,HIGH); 
+    delayMicroseconds(5);
     digitalWrite(PUL_PIN,HIGH);
     delayMicroseconds(de1);
     digitalWrite(PUL_PIN,LOW);
